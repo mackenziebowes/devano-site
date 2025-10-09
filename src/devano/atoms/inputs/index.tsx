b198a2714fa3fb539e34cb49e74fb554849499c6
Mackenzie Bowes <mackenzie@mackenziebowes.com>
2025-10-09 16:17:42 -0700

    Update index.tsx

--- src/devano/atoms/inputs/index.tsx
+++ src/devano/atoms/inputs/index.tsx
@@ -1,5 +1,6 @@
 import { CheckboxInput } from "./CheckboxInput";
 import { PasswordInput } from "./PasswordInput";
 import { TextInput } from "./TextInput";
+import { TextArea } from "./TextArea";
 
-export { CheckboxInput, PasswordInput, TextInput };
+export { CheckboxInput, PasswordInput, TextInput, TextArea };
