0a666ebc797d0eeddac6cdbc866721eb67d8c232
Mackenzie <mackenzie@mackenziebowes.com>
2025-10-28 10:44:24 -0700

    cleaned routes, builds in default state

--- src/global/index.tsx
+++ src/global/index.tsx
@@ -1,15 +1,12 @@
-import { AuthPortalProvider } from "./individual/Auth";
 import { JSX } from "solid-js";
 import { ScratchProvider } from "./individual/Scratch";
 import { AlertProvider } from "~/devano/components";
 interface ProviderProps extends JSX.HTMLAttributes<HTMLDivElement> {}
 
 export default function Contexts(props: ProviderProps) {
-	return (
-		<ScratchProvider>
-			<AlertProvider>
-				<AuthPortalProvider>{props.children}</AuthPortalProvider>
-			</AlertProvider>
-		</ScratchProvider>
-	);
+  return (
+    <ScratchProvider>
+      <AlertProvider>{props.children}</AlertProvider>
+    </ScratchProvider>
+  );
 }
