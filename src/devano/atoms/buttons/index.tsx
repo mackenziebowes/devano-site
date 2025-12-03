a1bf4b24070cd7d78d78d4e9c289d32f28136215
Mackenzie <mackenzie@mackenziebowes.com>
2025-12-02 16:06:37 -0800

    002

--- src/devano/atoms/buttons/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Anchor } from "./Anchor";
-import { AnchorButton } from "./AnchorButton";
-import { Button } from "./Button";
-import { ButtonAnchor } from "./ButtonAnchor";
-import { IconButton } from "./IconButton";
-
-export { Anchor, AnchorButton, Button, ButtonAnchor, IconButton };
--- src/devano/components/Button.tsx
+++ src/devano/components/Button.tsx
@@ -76,5 +76,3 @@ export default function Button(props: ButtonProps) {
     </button>
   );
 }
-
-const meow = <button class="transition-colors">Meow</button>;
